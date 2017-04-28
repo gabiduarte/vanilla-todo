48e25b67de537361f862556242bfc7e406cbd62d
Gabrielle Saman <gabi.duarte@gmail.com>
2017-04-28 02:11:08 -0300

    [#8] Create tests and configure test environment
    Changed SpecRunner.html to better debug and started redoing script.js to follow TDD

--- js/script.js
+++ js/script.js
@@ -1,61 +1,17 @@
 var Todo = {
     init: function() {
-        var todos = this.getTodos();
+        var todos = this.getFromLocalStorage('todos');
+        
         if (todos) {
-            this.create.tags(todos);
+            return todos;
         } else {
-            this.setTodos([]);
+            this.setToLocalStorage('todos', []);
         }
     },
-    getTodos: function() {
-        var todos = localStorage.getItem('todos');
-        return JSON.parse(todos);
+    getFromLocalStorage: function(item) {
+        return localStorage.getItem(item);
     },
-    setTodos: function(todos) {
-        localStorage.setItem('todos', JSON.stringify(todos));
-    },
-    create: {
-        todo: function() {
-            var description = document.getElementById('input').value;
-            var category = document.getElementById('category').value;
-            var todo = {description: description, category: category, isCompleted: false};
-
-            return this.todos(todo);
-        },
-        todos: function(todo) {
-            var todos = Todo.getTodos();
-            todos.push(todo);
-
-            return this.tags(todos);
-        },
-        tags: function(todos) {
-            var input = "";
-            var tag = "<tr><td><button type='button' class='fa-btn' onclick='Todo.delete.todo(this)'><i class='fa fa-trash-o fa-fw'></i></button><span class='flag flag-";
-
-            todos.forEach(function(e) {
-                input += tag + e.category + "'>" + e.category + "</span>"  + e.description + "</td></tr>";
-            });
-
-            return Todo.populate(input, todos);
-        }
-    },
-    populate: function(todoTags, todos) {
-        this.setTodos(todos);
-        document.getElementById('td-input').innerHTML = todoTags;
-    },
-    delete: {
-        todo: function(todo) {
-            var todoIndex = todo.parentElement.parentElement.rowIndex;
-            var todos = Todo.getTodos();
-            todos = todos.filter(item => todos.indexOf(item) !== todoIndex - 1);
-            Todo.create.tags(todos);
-        },
-        all: function() {
-            Todo.populate("", []);
-        }
+    setToLocalStorage: function(string, items) {
+        localStorage.setItem(string, JSON.stringify(items));
     }
-}
-
-window.onload = function() {
-    Todo.init();
 }
--- specs/VanillaSpec.js
+++ specs/VanillaSpec.js
@@ -1,5 +1,31 @@
 describe("Vanilla Todo", function() {
-	it("should be true", function() {
-		expect(true).toBeTruthy();
+
+	var fakeStorage;
+
+	beforeEach(function () {
+	  fakeStorage = {};
+
+	  spyOn(localStorage, 'getItem').and.callFake(function (key) {
+	    return fakeStorage[key];
+	  });
+	  spyOn(localStorage, 'setItem').and.callFake(function (key, value) {
+	    return fakeStorage[key] = value + '';
+	  });
+	});
+	
+	describe("Load Todos", function() {
+		it("creates an empty array in localStorage when it has no todos", function() {
+				expect(fakeStorage.todos).toBeUndefined();
+				Todo.init();
+			  expect(localStorage.getItem).toHaveBeenCalled();
+			  expect(fakeStorage.todos).toEqual('[]');
+		});
+
+		//TODO: Change implementation when tag creation is refactored
+		it("returns todo array when there is any todo in localStorage", function() {
+			localStorage.setItem("todos", "['A','B']");
+			var init = Todo.init();
+			expect(init).toEqual("['A','B']");
+		});
 	});
 });
